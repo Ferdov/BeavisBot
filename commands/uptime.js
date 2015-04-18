f2129ad69b8fa6bc8728f9f34da9edfbf9c5cd46
Ferdov <mineradorsim@gmail.com>
2015-04-18 19:48:35 -0300

    Delete uptime.js

--- commands/uptime.js
+++
@@ -1,7 +0,0 @@
-exports.names = ['.uptime'];
-exports.hidden = true;
-exports.enabled = true;
-exports.matchStart = false;
-exports.handler = function (data) {
-    bot.sendChat('up since ' + moment.utc(uptime.getTime()).calendar() + ' UTC (' + moment.utc(uptime.getTime()).fromNow() + ')');
-};

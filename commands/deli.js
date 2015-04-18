d466ea9382d79bdae42bea20ace533a8bc32ee34
Ferdov <mineradorsim@gmail.com>
2015-04-18 19:50:50 -0300

    Delete deli.js

--- commands/deli.js
+++
@@ -1,9 +0,0 @@
-exports.names = ['.deli'];
-exports.hidden = true;
-exports.enabled = true;
-exports.matchStart = false;
-exports.handler = function (data) {
-    users = bot.getUsers();
-    var randomNumber = _.random(1, users.length);
-    bot.sendChat(":bell: Now serving customer #" + randomNumber + " - hey there, " + users[(randomNumber - 1)].username + "!");
-};
